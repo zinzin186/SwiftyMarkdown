fc9d8b264604103c68d17020eb16cd0b4459f6bb
Simon Fairbairn <simon@line-in.co.uk>
2017-09-15 13:54:38 +0800

    Updating tests and example

--- SwiftyMarkdownExample/SwiftyMarkdownExample/ViewController.swift
+++ SwiftyMarkdownExample/SwiftyMarkdownExample/ViewController.swift
@@ -23,21 +23,23 @@ class ViewController: UIViewController {
 	@IBAction func reloadText( _ sender : UIButton? ) {
 				
 		self.textView.dataDetectorTypes = UIDataDetectorTypes.all
+		
+		if self.textView.text != "" {
+			self.textView.attributedText = SwiftyMarkdown(string: "Yo I'm a *single* line **string**. How do I look?").attributedString()
+			return
+		}
+
+		
 		if let url = Bundle.main.url(forResource: "example", withExtension: "md"), let md = SwiftyMarkdown(url: url) {
 			md.h2.fontName = "AvenirNextCondensed-Bold"
 			md.h2.color = UIColor.red
 			md.code.fontName = "CourierNewPSMT"
-			
+
 			self.textView.attributedText = md.attributedString()
-			
+
 		} else {
 			fatalError("Error loading file")
 		}
-	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
 	}
 }
 
