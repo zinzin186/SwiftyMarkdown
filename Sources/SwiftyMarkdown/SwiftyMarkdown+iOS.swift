aa748f2cddfbdfcc0e9dbb1df04040fc0c75021d
bensLine <bensLine@users.noreply.github.com>
2021-03-22 15:22:11 +0100

    Prefer configured font size over bold/italic descriptor size

--- Sources/SwiftyMarkdown/SwiftyMarkdown+iOS.swift
+++ Sources/SwiftyMarkdown/SwiftyMarkdown+iOS.swift
@@ -130,10 +130,10 @@ extension SwiftyMarkdown {
 		}
 		
 		if globalItalic, let italicDescriptor = font.fontDescriptor.withSymbolicTraits(.traitItalic) {
-			font = UIFont(descriptor: italicDescriptor, size: 0)
+			font = UIFont(descriptor: italicDescriptor, size: fontSize ?? 0)
 		}
 		if globalBold, let boldDescriptor = font.fontDescriptor.withSymbolicTraits(.traitBold) {
-			font = UIFont(descriptor: boldDescriptor, size: 0)
+			font = UIFont(descriptor: boldDescriptor, size: fontSize ?? 0)
 		}
 		
 		return font
